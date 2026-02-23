73cdf574295211e804ce060bbef5f88ec4b6baa8
Ilya Bobrov <ibobrov@ecom.tech>
2026-02-23 12:18:21 +0300

    Initial commit

---
+++ src/test/kotlin/i/bobrov/jpa_exmp/JpaExmpApplicationTests.kt
@@ -0,0 +1,13 @@
+package i.bobrov.jpa_exmp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaExmpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
